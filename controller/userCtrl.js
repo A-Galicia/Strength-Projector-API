1fe591f9b7a91d7d8a9315619b7f5bb52bbab00c
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2025-07-29 14:40:22 -0700

    added user login and provide jwt

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,5 +1,32 @@
-const { PrismaClient } = require('../generated/prisma');
+require('dotenv').config();
+const passport = require('passport');
 const bcrypt = require('bcryptjs');
+const { PrismaClient } = require('../generated/prisma');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
+
+// JWT Config /////////////////////////////////////////////////////
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+passport.use(
+  new JwtStrategy(opts, async (payload, done) => {
+    try {
+      const user = prisma.user.findFirst({
+        where: {
+          id: payload.id,
+        },
+      });
+      if (user) return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
 
 //_________________________________________________________________
 
@@ -21,6 +48,48 @@ class AuthCtrl {
       res.status(409).json({ message: 'Error: username or email is taken' });
     } else {
       res.status(201).json({ message: 'Created User', user: user });
+    }
+  };
+
+  login = async (req, res, next) => {
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          OR: [
+            { name: req.body.usernameOrEmail },
+            { email: req.body.usernameOrEmail },
+          ],
+        },
+      });
+
+      if (!user)
+        return res.status(400).json({ message: 'user does not exist' });
+
+      if (
+        user.name !== req.body.usernameOrEmail &&
+        user.email !== req.body.usernameOrEmail
+      )
+        return res.status(400).json({ message: 'incorrect username or email' });
+
+      const match = await bcrypt.compare(req.body.password, user.password);
+      if (!match)
+        return res.status(400).json({ message: 'incorrect password' });
+
+      const accessToken = jwt.sign(
+        {
+          id: user.id,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '4h' }
+        //expires in 4 hours, should last whole workout
+      );
+
+      return res
+        .status(200)
+        .json({ message: 'user logged in', accessToken: accessToken });
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
   };
 }
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,5 +4,6 @@ const authCtrl = require('../controller/userCtrl.js');
 const router = Router();
 
 router.post('/api/user', authCtrl.createUser);
+router.post('/api/login', authCtrl.login);
 
 module.exports = router;
