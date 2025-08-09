0268bd5e9accb6328b6d817944842f6c104f4bf1
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2025-08-09 09:52:19 -0700

    deleted cors origin

--- app.js
+++ app.js
@@ -12,10 +12,9 @@ app.use(nocache());
 //app.options('*', cors());
 app.use(
   cors({
-    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE, OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    preflightContinue: false,
+    preflightContinue: true,
     optionsSuccessStatus: '200',
     credentials: true,
   })
