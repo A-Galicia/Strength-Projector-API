06630442603c28aff17953951399e10da6c02905
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2025-08-09 09:49:20 -0700

    disabled preflight continue

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE, OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    preflightContinue: true,
+    preflightContinue: false,
     optionsSuccessStatus: '200',
     credentials: true,
   })
